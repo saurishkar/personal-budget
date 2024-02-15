04f7a3f8ff8a89d712f62f1107a670bfcf5cba61
Saurish Kar <basicallysaurish@gmail.com>
2024-02-16 00:06:27 +0530

    added delete feature

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const envelopes = [];
 let envelopeId = 1;
 
-const createEnvelope = ({ name, budget }) => {
-    envelopes.push({ name, budget, id: envelopeId++ });
+const createEnvelope = ({ name, balance = 0 }) => {
+    envelopes.push({ name, balance, id: envelopeId++ });
     return envelopes[envelopes.length - 1];
 }
 
@@ -10,17 +10,50 @@ const getAllEnvelopes = () => {
     return envelopes;
 }
 
+const getEnvelopeIndexFromId = (envelopeId) => {
+    return envelopes.findIndex((record) => record.id === Number(envelopeId));
+}
+
 const getEnvelopeById = (envelopeId) => {
-    const envelopeIndex = envelopes.findIndex((record) => record.id === Number(envelopeId));
-    console.log(envelopeIndex);
+    const envelopeIndex = getEnvelopeIndexFromId(envelopeId);
     if(envelopeIndex === -1) {
         return;
     }
     return envelopes[envelopeIndex];
 }
 
+const isEnvelopeInstanceValid = (instance) => {
+    if(!instance.id) {
+        return false;
+    }
+    if(!instance.name.trim()) {
+        return false;
+    }
+    if(!instance.balance) {
+        return false;
+    }
+    if(isNaN(Number(instance.moneySpent))) {
+        return false;
+    }
+    return true;
+}
+
+const updateEnvelope = (envelopeId, envelopeInstance) => {
+    const envelopeIndex = getEnvelopeIndexFromId(envelopeId);
+    envelopes[envelopeIndex] = envelopeInstance;
+    return envelopes[envelopeIndex];
+}
+
+const deleteEnvelope = (envelopeId) => {
+    const envelopeIndex = getEnvelopeIndexFromId(envelopeId);
+    envelopes.splice(envelopeIndex, 1);
+    return envelopes;
+}
+
 module.exports = {
     createEnvelope,
+    updateEnvelope,
     getAllEnvelopes,
-    getEnvelopeById
+    getEnvelopeById,
+    deleteEnvelope
 }
--- server/routes/envelopes.js
+++ server/routes/envelopes.js
@@ -1,31 +1,55 @@
 const express = require('express');
-const { createEnvelope, getAllEnvelopes, getEnvelopeById } = require("../db");
+const { createEnvelope, updateEnvelope, getAllEnvelopes, getEnvelopeById, deleteEnvelope } = require("../db");
 
 const envelopeRouter = express();
 
 envelopeRouter.post("/", (req, res, next) => {
-    const { name = "", budget = 0 } = req.body;
-    if(!name.trim() || isNaN(Number(budget))) {
+    const { name = "", balance = 0 } = req.body;
+    if(!name.trim() || isNaN(Number(balance))) {
         res.status(400).send("Invalid values provided");
         return;
     }
-    if(budget  <= 0) {
-        res.status(400).send("Budget should be a positive number");
+    if(balance  <= 0) {
+        res.status(400).send("Balance should be a positive number");
     }
-    res.send(createEnvelope({ name, budget }));
+    res.status(201).send(createEnvelope({ name, balance }));
 })
 
 envelopeRouter.get("/", (req, res, next) => {
     res.send(getAllEnvelopes());
 });
 
-envelopeRouter.get("/:envelopeId", (req, res, next) => {
+envelopeRouter.param("envelopeId", (req, res, next, envelopeId) => {
     const envelope = getEnvelopeById(req.params.envelopeId);
-    if(envelope) {
-        res.send(envelope);
+    if(!envelope) {
+        res.status(404).send("Envelope not found");
         return;
     }
-    res.status(404).send("Envelope not found");
+    req.envelope = envelope;
+    next();
+})
+
+envelopeRouter.get("/:envelopeId", (req, res, next) => {
+    res.send(req.envelope);    
+})
+
+envelopeRouter.put("/:envelopeId", (req, res, next) => {
+    const { balance, name } = req.body;
+    const envelope = req.envelope;
+    if(typeof balance === "number" && balance < 0) {
+        res.status(400).send("Balance should be a positive number");
+        return;
+    }
+    const instance  = {
+        ...envelope,
+        balance: balance || envelope.balance,
+        name: name || envelope.name,
+    }
+    res.send(updateEnvelope(req.params.envelopeId, instance));
+})
+
+envelopeRouter.delete("/:envelopeId", (req, res, next) => {
+    res.send(204).send(deleteEnvelope(req.params.envelopeId))
 })
 
 module.exports = envelopeRouter;
