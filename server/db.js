f9e42f0dd27fae965a5b57cb42580a52df42c7bc
Saurish Kar <basicallysaurish@gmail.com>
2024-02-15 16:20:13 +0530

    add an id property while creating envelope

--- server/db.js
+++ server/db.js
@@ -1,14 +1,15 @@
 const envelopes = [];
-
+let envelopeId = 1;
 
 const createEnvelope = ({ name, budget }) => {
-    envelopes.push({ name, budget });
+    envelopes.push({ name, budget, id: envelopeId++ });
     return envelopes[envelopes.length - 1];
 }
 
 const getAllEnvelopes = () => {
     return envelopes;
 }
+
 
 module.exports = {
     createEnvelope,
